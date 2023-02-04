e87cd3d7fea9b5ebe7b315177616535f83d324c5
ishikawa tatsuya <stone.guitar.777@gmail.com>
2023-02-04 10:45:08 +0900

    js refactoring

--- src/Blazor.DynamicJS/wwwroot/blazor.dynamicjs/helper.js
+++ src/Blazor.DynamicJS/wwwroot/blazor.dynamicjs/helper.js
@@ -1,19 +1,19 @@
 ﻿window.BlazorDynamicJavaScriptHelper =
 {
-    index: 0,
+    objectIdCreate: 0,
     objects: {},
 
-    invokeMethod: function (id, names, theArgs) {
+    invokeMethod: function (objId, names, theArgs) {
 
         window.BlazorDynamicJavaScriptHelper.resolveArgs(theArgs);
 
-        if (names.length == 0 && id != 0) {
+        if (names.length == 0 && objId != 0) {
             //functionが入っている場合
-            const obj = window.BlazorDynamicJavaScriptHelper.objects[id](...theArgs);
+            const obj = window.BlazorDynamicJavaScriptHelper.objects[objId](...theArgs);
             return window.BlazorDynamicJavaScriptHelper.setObject(obj);
         }
         else {
-            const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(id, names);
+            const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(objId, names);
             const obj = info.target == null ?
                 window[info.last].apply(info.target, theArgs) :
                 info.target[info.last].apply(info.target, theArgs);
@@ -21,47 +21,47 @@
         }
     },
 
-    setProperty: function (id, names, obj) {
+    setProperty: function (objId, names, obj) {
         var vals = [obj];
         window.BlazorDynamicJavaScriptHelper.resolveArgs(vals);
         obj = vals[0];
 
 
-        const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(id, names);
+        const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(objId, names);
 
         if (info.target === null) window[info.last] = obj;
         else info.target[info.last] = obj;
     },
 
-    getIndex: function (id, names, index) {
-        if (names.length == 0) return window.BlazorDynamicJavaScriptHelper.objects[id];
-        const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(id, names);
+    getIndex: function (objId, names, index) {
+        if (names.length == 0) return window.BlazorDynamicJavaScriptHelper.objects[objId];
+        const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(objId, names);
         var obj = (info.target == null) ? window[info.last][index] : info.target[info.last][index];
         return window.BlazorDynamicJavaScriptHelper.setObject(obj);
     },
 
-    setIndex: function (id, names, index, obj) {
+    setIndex: function (objId, names, index, obj) {
         var vals = [obj];
         window.BlazorDynamicJavaScriptHelper.resolveArgs(vals);
         obj = vals[0];
 
 
-        const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(id, names);
+        const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(objId, names);
 
         if (info.target === null) window[info.last][index] = obj;
         else info.target[info.last][index] = obj;
     },
 
-    getObject: function (id, names) {
-        if (names.length == 0) return window.BlazorDynamicJavaScriptHelper.objects[id];
-        const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(id, names);
+    getObject: function (objId, names) {
+        if (names.length == 0) return window.BlazorDynamicJavaScriptHelper.objects[objId];
+        const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(objId, names);
         return (info.target == null) ? window[info.last] : info.target[info.last];
     },
 
     setObject: function (obj) {
-        const id = ++window.BlazorDynamicJavaScriptHelper.index;
-        window.BlazorDynamicJavaScriptHelper.objects[id] = obj;
-        return id;
+        const objId = ++window.BlazorDynamicJavaScriptHelper.objectIdCreate;
+        window.BlazorDynamicJavaScriptHelper.objects[objId] = obj;
+        return objId;
     },
 
     createObject: function (names, theArgs) {
@@ -86,13 +86,13 @@
     resolveArgs: function (theArgs) {
         for (let i = 0; i < theArgs.length; i++) {
             if (theArgs[i].hasOwnProperty("blazorDynamicJavaScriptObjectId")) {
-                var id = theArgs[i]["blazorDynamicJavaScriptObjectId"];
+                var objId = theArgs[i]["blazorDynamicJavaScriptObjectId"];
                 var names = theArgs[i]["blazorDynamicJavaScriptUnresolvedNames"];
 
                 if (names.length == 0) {
-                    theArgs[i] = window.BlazorDynamicJavaScriptHelper.objects[id];
+                    theArgs[i] = window.BlazorDynamicJavaScriptHelper.objects[objId];
                 } else {
-                    const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(id, names);
+                    const info = window.BlazorDynamicJavaScriptHelper.getInvokeInfo(objId, names);
                     const obj = (info.target == null) ? window[info.last] : info.target[info.last];
                     theArgs[i] = obj;
                 }
@@ -100,12 +100,12 @@
         }
     },
 
-    getInvokeInfo: function (id, names) {
+    getInvokeInfo: function (objId, names) {
         const last = names.slice(-1)[0];
         const others = names.slice(0, -1);
         let target = null;
-        if (id != 0) {
-            target = window.BlazorDynamicJavaScriptHelper.objects[id];
+        if (objId != 0) {
+            target = window.BlazorDynamicJavaScriptHelper.objects[objId];
         }
         for (let e of others) {
             if (target == null) target = window[e];
